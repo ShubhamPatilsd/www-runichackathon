a52665ed781b8f7f71d519fb668e6e115734bee0
Shubham Patil <shubhampatilsd@gmail.com>
2023-11-20 08:56:41 -0800

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import Meta from "@/components/Meta";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Head } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Meta as={Head} />
+      <Analytics />
       <Component {...pageProps} />
     </div>
   );
