a66a86c409543cd7d1316e7b31d94ba655a3a6e2
Shubham Patil <shubhampatilsd@gmail.com>
2023-12-05 17:55:43 -0800

    sonsors hcb

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -8,12 +8,12 @@ export const Sponsors = () => {
         <h1 className="text-3xl md:text-4xl text-left px-2 mb-4">Sponsors</h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4	px-[10vw] md:px-2">
-          <SponsorBox
+          {/* <SponsorBox
             header={"HCB"}
             text={"Allows us to manage our finances and make Runic work!"}
             imageUrl={`https://assets.hackclub.com/hcb-dark.svg`}
             link={"https://hcb.hackclub.com"}
-          />
+          /> */}
           {/* <a href="mailto:sponsor@runic.rsvp"> */}
           <div className="flex-col flex items-center p-6 w-full border-2 rounded-md border-accent border-dashed h-full aspect-square">
             <FaPlus className="text-[9vw] md:text-[5vw] lg:text-[4vw]" />
